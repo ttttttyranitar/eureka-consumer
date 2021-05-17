6af932ad90e16f46e35d756b48caa6e7fcb58850
sunriuqi <sunriuqi@hatech.com.cn>
2021-05-17 10:11:11 +0800

    first commit version 1.0.0

---
+++ src/main/java/com/demo/eureka/eurekaconsumer/config/RibbonConfig.java
@@ -0,0 +1,50 @@
+/**
+ * copyright (C), 2021, ImagineAltaria
+ *
+ * @program eureka-consumer
+ * @description
+ * @author liuda
+ * @create 2021/5/17 9:24
+ * @version 1.0.0
+ * <author>                <time>                  <version>                   <description>
+ * liuda         2021/5/17 9:24             1.0
+ */
+
+package com.demo.eureka.eurekaconsumer.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import io.netty.handler.ipfilter.IpFilterRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
+
+@Configuration
+public class RibbonConfig {
+
+
+    /**
+     * 配置客户端远程服务调用方式
+     *
+     * */
+    @Bean
+    @LoadBalanced
+    public RestTemplate getTemplate(){
+
+        return new RestTemplate();
+    }
+
+    /**
+     * 配置负载均衡算法，采用轮询方式
+     *
+     *
+     * **/
+    @Bean
+    public IRule getLoadBalanceRule(){
+
+        return new RoundRobinRule();
+    }
+}
---
+++ src/main/java/com/demo/eureka/eurekaconsumer/controller/ConsumerController.java
@@ -0,0 +1,85 @@
+/**
+ * copyright (C), 2021, ImagineAltaria
+ *
+ * @program eureka-consumer
+ * @description
+ * @author liuda
+ * @create 2021/5/13 9:22
+ * @version 1.0.0
+ * <author>                <time>                  <version>                   <description>
+ * liuda         2021/5/13 9:22             1.0
+ */
+
+package com.demo.eureka.eurekaconsumer.controller;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    EurekaClient client;
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
+    @Autowired
+    LoadBalancerClient lb;
+
+
+
+    @GetMapping("/getHelloMS")
+    public Object getHelloMs(){
+        List<ServiceInstance> instances = client.getInstancesById("helloMS");
+        List<URI> uriList = instances.stream().map(instance -> {
+            return instance.getUri();
+        }).collect(Collectors.toList());
+
+        System.out.println(uriList);
+        return uriList ;
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        List<InstanceInfo> instances = client.getInstancesByVipAddress("helloMS", false);
+        String res="";
+        for (InstanceInfo instanceInfo:instances){
+
+            if (InstanceInfo.InstanceStatus.UP==instanceInfo.getStatus()){
+
+                String url="http://"+"localhost:"+instanceInfo.getPort()+"/hello";
+                System.out.println("use service instance : "+url);
+
+                RestTemplate restTemplate = restTemplateBuilder.build();
+                res = restTemplate.getForObject(url, String.class);
+                break;
+            }
+        }
+        return res;
+    }
+
+    @GetMapping("/hello2")
+    public String hello2(){
+
+        ServiceInstance instance = lb.choose("helloMS");
+
+        String url="http://"+"localhost:"+instance.getPort()+"/hello";
+
+        String res = restTemplateBuilder.build().getForObject(url, String.class);
+        System.out.println(url);
+        return res;
+    }
+}
